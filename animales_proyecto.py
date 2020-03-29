a2012cc3ada6e5488197244162edd7b88b114b11
Oscar Manuel Ruiz Reyes <34193718+OscarManuelRuizReyes@users.noreply.github.com>
2020-03-28 21:46:51 -0500

    Proyecto elaborado en jetbrains
    Conceptos fundamentales de python 3

---
+++ animales_proyecto.py
@@ -0,0 +1,129 @@
+camello = """
+Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!"""
+
+leon = """
+Switching on camera from habitat with lions...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is croaking!"""
+
+ciervo = """
+Switching on camera from habitat with deers...
+   /|       |\\
+`__\\\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \\
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+ganzo = """
+Switching on camera from habitat with lovely goose...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \\
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \\
+ <_  `     (  <`<            \              `-._\\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+This bird stares intently at you... (Maybe it's time to change the channel?)"""
+
+murcielago = """
+Switching on camera from habitat with bats...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\\
+                ~-. /  \_/  \ .-~
+                   V         V
+It looks like this bat is fine."""
+
+conejo = """
+Switching on camera from habitat with rabbits...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\//\/      .- \\
+ Y        /    Y 
+ l       I     !
+ ]\      _\    /"\\
+(" ~----( ~   Y.  )
+It seems there will be more rabbits soon!"""
+
+animals = [camello, leon, ciervo, ganzo, murcielago, conejo]
+res = ""
+while True:
+   res = input("que habitad # necesitas? (del 0 al 5), escribe exit para salir > ")
+   if res.lower() == "exit":
+      break
+   print(animals[int(res)])
+print("Hasta la proxima vaquero!")
